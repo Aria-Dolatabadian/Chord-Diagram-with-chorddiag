9bb9bcd29f2252f591bec66ce0bb86bdc908ad03
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-23 16:06:18 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,3 +1,11 @@
+library(tidyverse)
+library(viridis)
+library(patchwork)
+library(hrbrthemes)
+library(circlize)
+library(chorddiag)
+
+
 m <- matrix(c(11975,  5871, 8916, 2868,1000,
               1951, 10048, 2060, 6171,1000,
               8010, 16145, 8090, 8045,1000,
@@ -9,7 +17,7 @@ chromosome <- c("A1", "A2", "A3", "A4", "A5")
 dimnames(m) <- list(have = chromosome,
                     prefer = chromosome)
 
-library(chorddiag)
+
 groupColors <- c("#000000", "#FFDD89", "#957244", "#F26223","#FFDD02")
 chorddiag(m, groupColors = groupColors, groupnamePadding = 50)
 
